caf25e2d7be06e42daafdf4f9347640e5637f8f1
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-03-17 23:52:18 +0600

    assginment 02 repeatString done.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
+const repeatString = function(repeatThis, numberOfRepeat ) {
 
+        let result="";
+        for(let i=0; i< numberOfRepeat; i++){
+            
+            result= result+repeatThis;
+            }
+        return result;
 };
+ 
 
 // Do not edit below this line
 module.exports = repeatString;
