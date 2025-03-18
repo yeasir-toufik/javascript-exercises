f89f0c59e7a8f56a156d76b18722572603c55efc
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-03-18 16:22:45 +0600

    assignment 07_tempConversion done.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenhite) {
+
+  let celsius= (farenhite-32)*(5/9);
+  let result=Math.round(celsius*10)/10;
+  return result;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+    let farenhite= celsius*(9/5)+32;
+    let result=Math.round(farenhite*10)/10;
+    return result;
 };
 
 // Do not edit below this line
