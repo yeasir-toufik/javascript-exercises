9e10dd7c014861410b5715a6be44da7e9830b834
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-03-18 15:05:07 +0600

    assginment 04 removeFromArray done.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function( userArray, ...removeItem ) {
+
+   
+
+    return userArray.filter(x => !removeItem.includes(x));
+
 };
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
