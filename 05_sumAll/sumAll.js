dc48130bd3ebbbbb8ca8a478b2d77c0393f15e4e
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-03-18 15:43:09 +0600

    assignment 05 sumAll done.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,40 @@
-const sumAll = function() {
+const sumAll = function( num1, num2) {
+
+    let sumOfAllInteger=0;
+
+    if( ((num1 || num2)>=0) && (Number.isInteger(num1) && Number.isInteger(num2)) ) {
+
+        if (num1 < num2) {
+
+            for (let i = num1; i <= num2; i++) {
+
+                sumOfAllInteger = sumOfAllInteger + i;
+
+            }
+
+            return sumOfAllInteger;
+        }
+
+        else if(num1>num2){
+
+            for (let i = num2; i <= num1; i++) {
+
+                sumOfAllInteger = sumOfAllInteger + i;
+
+            }
+
+            return sumOfAllInteger;
+
+        }
+        else{ return num1;}
+
+    }
+
+    else{
+        return "ERROR";
+    }
+
+    
 
 };
 
