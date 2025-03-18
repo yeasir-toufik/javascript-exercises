6fe4ebed5cbd8ecaa9b721b568005cfd985bbe85
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-03-18 13:58:05 +0600

    assignment 03 reverseString done.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,20 @@
-const reverseString = function() {
+const reverseString = function ( userInput ) {
+
+    
+    let newString = "";
+    for (let i = userInput.length - 1; i >= 0; i--) {
+        newString = newString+ userInput[i];
+    }
+    return newString;
+    
+    
+    
+    // const ans = userInput.split('').reverse().join('');
+    // return ans;
+    
+
+
+
 
 };
 
