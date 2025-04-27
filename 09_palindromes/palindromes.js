bf6d7a7b13dae48220ae151a25be1e2856fc7061
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-04-28 01:07:41 +0600

    palindromes assignment done.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (data) {
 
+
+    let str = data.split("");
+
+
+    let str2 = str.filter(l => l.match(/[a-z|0-9]/i)).join("");
+    str2 = str2.toLowerCase();
+
+    let final = str2.split('').reverse().join('');
+
+    if (str2 == final) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+
+let result= palindromes("racecar");
+
 
 // Do not edit below this line
 module.exports = palindromes;
