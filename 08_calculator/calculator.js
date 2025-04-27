51b6e161b21c3b19f401bdf05cd74132474d1258
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-04-27 23:45:09 +0600

    calculator assignment doen.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
+const add = function(a,b) {
+	return a+b;
+};
+
+const subtract = function(a,b) {
+	return a-b;
+};
+
+const sum = function(arr) {
+	return arr.reduce((acc,curr)=>acc+curr,0);
+};
+
+const multiply = function(arr) {
+  return arr.reduce((acc,curr)=>acc*curr);
+};
+
+const power = function(a,b) {
+  return Math.pow(a,b);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num==0){
+    return 1;
+  }
+  else if (num !=0){
+    let result=1;
+    for (let i=num; i>=1;i--){
+      result= result*i;     
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
