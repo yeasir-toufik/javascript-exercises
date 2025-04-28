251146e750f35786c9fd41fec7b42da3c2b351f5
Yeasir Arafat <yeasir.toufik@gmail.com>
2025-04-28 11:35:50 +0600

    fibonacci assignment done.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let n1=0,n2=1,next=0;
+
+    if (number < 0) {
+        return ("OOPS");
+    }
+    else if (number==1){
+        return 1;
+    }
+
+    else if (number==0){
+        return 0; 
+    }
+    else if (number > 1) {
+        for (let i = 1; i < number; i++) {
+            next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+        return next;
+    }
+    
 
 };
 
